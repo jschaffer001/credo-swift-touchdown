528ed47d004b3370ddfecc11904e9ae779f8a0f4
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-23 16:17:16 -0700

    Added the a product grid view to the main content view. The view dynamically sources the target products from the reference JSON file using a new product data model and the existing JSON parcer.

---
+++ Touchdown/View/Home/ProductItemView.swift
@@ -0,0 +1,51 @@
+// ProductItemView.swift
+// Touchdown
+// 
+// Created by Jonathan Schaffer
+// Using Swift 5.0
+//
+// https://jonathanschaffer.com
+// Copyright © 2023 Jonathan Schaffer. All rights reserved.
+
+import SwiftUI
+
+struct ProductItemView: View {
+    // MARK: - PROPERTIES
+    let product: Product
+    
+    // MARK: - BODY
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6, content: {
+            // PHOTO
+            ZStack {
+                Image(product.image)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(10)
+            } //: ZSTACK
+            .background(Color(red: product.red, green: product.green, blue: product.blue))
+            .cornerRadius(12)
+            
+            // NAME
+            Text(product.name)
+                .font(.title3)
+                .fontWeight(.black)
+            
+            // PRICE
+            Text(product.formattedPrice)
+                .fontWeight(.heavy)
+                .foregroundColor(.gray)
+            
+        }) //: VSTACK
+    }
+}
+
+// MARK: - PREVIEW
+struct ProductItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductItemView(product: products[0])
+            .previewLayout(.fixed(width: 200, height: 300))
+            .padding()
+            .background(colorBackground)
+    }
+}
---
+++ Touchdown/View/Home/TitleView.swift
@@ -0,0 +1,36 @@
+// TitleView.swift
+// Touchdown
+// 
+// Created by Jonathan Schaffer
+// Using Swift 5.0
+//
+// https://jonathanschaffer.com
+// Copyright © 2023 Jonathan Schaffer. All rights reserved.
+
+import SwiftUI
+
+struct TitleView: View {
+    // MARK: - PROPERTIES
+    var title: String
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.largeTitle)
+            .fontWeight(.heavy)
+        } //: HSTACK
+        .padding(.horizontal)
+        .padding(.top, 15)
+        .padding(.bottom, 10)
+    }
+}
+
+// MARK: - PREVIEW
+struct TitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleView(title: "Helmet")
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
