254a11effb1d8de4993ff6e1b63ed3dc928d41e1
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-23 12:03:37 -0700

    Initial commit

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,39 @@
+// Constant.swift
+// Touchdown
+// 
+// Created by Jonathan Schaffer
+// Using Swift 5.0
+//
+// https://jonathanschaffer.com
+// Copyright © 2023 Jonathan Schaffer. All rights reserved.
+
+import SwiftUI
+
+// DATA
+
+
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+
+// UX
+
+
+// API
+
+
+// IMAGE
+
+
+// FONT
+
+
+// STRING
+
+
+// MISC
+
+
