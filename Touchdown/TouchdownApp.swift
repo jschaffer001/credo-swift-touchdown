9f75b28babbc0ed7e56ad3b7b8f5eb28663eef75
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-23 10:40:28 -0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Jonathan Schaffer on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
