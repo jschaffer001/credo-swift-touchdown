f8143021471cc219f4cab7685e4a1778efa1e93d
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-23 13:42:07 -0700

    Added both the footer and the animated header with search and shopping cart buttons stubbed for future dev.

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,43 @@
+// LogoView.swift
+// Touchdown
+// 
+// Created by Jonathan Schaffer
+// Using Swift 5.0
+//
+// https://jonathanschaffer.com
+// Copyright © 2023 Jonathan Schaffer. All rights reserved.
+
+import SwiftUI
+
+struct LogoView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        } //: HSTACK
+    }
+}
+
+// MARK: - PREVIEW
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
